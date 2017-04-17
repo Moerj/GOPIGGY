1be0bed725249aeae6177e0e66b3adfa68d74195
Moer <199525807@qq.com>
2017-04-17 16:06:59 +0800

    修正嵌套路由不正确

--- src/router/router-index.js
+++ src/router/router-index.js
@@ -6,13 +6,14 @@ import index from 'components/index'
 
 // 导出路由配置
 export default {
+    // 所有path都要以 / 开头,否则嵌套路由会出问题
     path: '/',
     name: 'index',
     component: index,
     children: [
         // 首页
         {
-            path: 'home', // ../#/home
+            path: '/home', // ../#/home
             name: 'home',
             component: resolve => require(['components/home'], resolve)
         },
@@ -20,12 +21,12 @@ export default {
         // 交易
         {
             // 所有订单
-            path: 'orders_all',
+            path: '/orders_all',
             name: 'orders_all',
             component: resolve => require(['components/orders/orders_all'], resolve)
         }, {
             // 订单详情
-            path: 'orders_details/:index',
+            path: '/orders_details/:index',
             name: 'orders_details',
             component: resolve => require(['components/orders/orders_details'], resolve)
         },
@@ -33,7 +34,7 @@ export default {
         // 消息
         {
             // 站内信
-            path: 'mail',
+            path: '/mail',
             name: 'mail',
             component: resolve => require(['components/mail'], resolve)
         },
@@ -41,9 +42,17 @@ export default {
         // 财务
         {
             // 我的账户
-            path: 'my_account',
+            path: '/my_account',
             name: 'my_account',
-            component: resolve => require(['components/finance/my_account'], resolve)
+            component: resolve => require(['components/finance/my_account'], resolve),
+            children: [
+                // 会员资金
+                {
+                    path: '/member_funds',
+                    name: 'member_funds',
+                    component: resolve => require(['components/finance/member_funds'], resolve),
+                }
+            ]
         },
     ]
 }
