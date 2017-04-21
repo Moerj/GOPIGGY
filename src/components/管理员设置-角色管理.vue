6fb76a2930ffaaa418e71fb2b9152e0c2ac608ff
Moer <199525807@qq.com>
2017-04-21 17:58:59 +0800

    管理员设置

---
+++ src/components/管理员设置-角色管理.vue
@@ -0,0 +1,114 @@
+<style lang="scss" scoped>
+    .el-menu {
+        background-color: transparent;
+    }
+</style>
+<template>
+    <div>
+        <div>
+            <el-button class="m-b-15" @click="showDialogUserAdd=true">添加角色</el-button>
+            <el-button type="primary" class="m-b-15">保存</el-button>
+        </div>
+        <div class="flex">
+            <div style="width:150px;height: 100%;" class="ui-card">
+                <el-menu mode="vertical" default-active="">
+                    <el-menu-item-group title="所有角色">
+                        <el-menu-item :index="item.id" v-for="item in userAllData">{{item.name}}</el-menu-item>
+                    </el-menu-item-group>
+                </el-menu>
+            </div>
+            <div class="grow-1 m-l-15">
+                <el-tree :data="currentTreeData" show-checkbox default-expand-all node-key="id" ref="tree" highlight-current :props="currentTreeProps" class="ui-border p-15">
+                </el-tree>
+            </div>
+        </div>
+
+        <el-dialog title="添加角色" v-model="showDialogUserAdd">
+            <el-form :model="userAddData" class="ui-dialog">
+                <el-form-item label="角色名" label-width="120px">
+                    <el-input v-model="userAddData.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="id" label-width="120px">
+                    <el-input v-model="userAddData.id" auto-complete="off"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="showDialogUserAdd = false">取 消</el-button>
+                <el-button type="primary" @click="showDialogUserAdd = false; saveUserAddData()">保 存</el-button>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                userAllData: [{
+                    name: '管理员1',
+                    id: '1'
+                }, {
+                    name: '管理员2',
+                    id: '2'
+                }],
+                showDialogUserAdd: false,
+                userAddData: {
+                    name: '',
+                    id: ''
+                },
+                currentTreeData: [{
+                    id: 1,
+                    label: '一级 1',
+                    children: [{
+                        id: 4,
+                        label: '二级 1-1',
+                        children: [{
+                            id: 9,
+                            label: '三级 1-1-1'
+                        }, {
+                            id: 10,
+                            label: '三级 1-1-2'
+                        }]
+                    }]
+                }, {
+                    id: 2,
+                    label: '一级 2',
+                    children: [{
+                        id: 5,
+                        label: '二级 2-1'
+                    }, {
+                        id: 6,
+                        label: '二级 2-2'
+                    }]
+                }, {
+                    id: 3,
+                    label: '一级 3',
+                    children: [{
+                        id: 7,
+                        label: '二级 3-1'
+                    }, {
+                        id: 8,
+                        label: '二级 3-2'
+                    }]
+                }],
+                currentTreeProps: {
+                    children: 'children',//指定子树为节点对象的某个属性值
+                    label: 'label'//指定节点标签为节点对象的某个属性值
+                }
+            }
+        },
+        methods: {
+            saveUserAddData() {
+                if (this.userAddData.name) {
+                    this.$message({
+                        message: '新增角色: ' + this.userAddData.name,
+                        type: 'success'
+                    });
+                    this.userAllData.push(this.userAddData)
+                    this.userAddData = {}
+                } else {
+                    this.$message.error('新增角色失败');
+                }
+            }
+        }
+    }
+</script>
--- src/components/管理员设置.vue
+++ src/components/管理员设置.vue
@@ -2,8 +2,10 @@
     <div>
         <el-tabs v-model="activeTab" type="card" @tab-click="tabChange">
             <el-tab-pane label="管理员" name="admin">
-                <el-button type="primary" class="m-b-15"><i class="fa fa-plus m-r-5"></i>新增管理员</el-button>
-                <el-table :data="admin.data"  style="width: 100%">
+
+                <el-button type="primary" class="m-b-15" @click="showAddDialog=true"><i class="fa fa-plus m-r-5"></i>新增管理员</el-button>
+
+                <el-table :data="admin.data" style="width: 100%">
                     <el-table-column prop="accout" label="账号">
                     </el-table-column>
                     <el-table-column prop="name" label="昵称" width="180">
@@ -29,36 +31,94 @@
                 </el-pagination>
             </el-tab-pane>
 
-            <el-tab-pane label="角色" name="user">配置管理</el-tab-pane>
+            <el-tab-pane label="角色管理" name="user">
+                <userManage></userManage>
+            </el-tab-pane>
         </el-tabs>
+
+
+        <!--新增管理员-->
+        <el-dialog title="新增管理员" v-model="showAddDialog">
+            <el-form :model="admin.addData" style="width:500px;">
+                <el-form-item label="账号" :label-width="formLabelWidth">
+                    <el-input placeholder="" v-model="admin.addData.name">
+                        <template slot="prepend">admin:</template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="密码" :label-width="formLabelWidth">
+                    <el-input type="password" placeholder="" v-model="admin.addData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" :label-width="formLabelWidth">
+                    <el-input type="password" placeholder="" v-model="admin.addData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="姓名" :label-width="formLabelWidth">
+                    <el-input placeholder="" v-model="admin.addData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="性别" :label-width="formLabelWidth">
+                    <el-radio v-model="admin.addData.sex" label="男">男</el-radio>
+                    <el-radio v-model="admin.addData.sex" label="女">女</el-radio>
+                    <el-radio v-model="admin.addData.sex" label="保密">保密</el-radio>
+                </el-form-item>
+                <el-form-item label="人人店后台登录微信" :label-width="formLabelWidth">
+                    <el-input placeholder="" v-model="admin.addData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号" :label-width="formLabelWidth">
+                    <el-input placeholder="" v-model="admin.addData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="年龄" :label-width="formLabelWidth">
+                    <el-input placeholder="" v-model="admin.addData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="角色" :label-width="formLabelWidth">
+                    <el-checkbox v-model="admin.addData.postion" label="平台版">平台版</el-checkbox>
+                    <el-checkbox v-model="admin.addData.postion" label="运营">运营</el-checkbox>
+                    <el-checkbox v-model="admin.addData.postion" label="发货员">发货员</el-checkbox>
+                    <el-checkbox v-model="admin.addData.postion" label="市场">市场</el-checkbox>
+                </el-form-item>
+                <el-form-item label="虚拟商品验证权限" :label-width="formLabelWidth">
+                    <el-radio name="getVaild" v-model="admin.addData.getVaild" label="是">是</el-radio>
+                    <el-radio name="getVaild" v-model="admin.addData.getVaild" label="否">否</el-radio>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="showAddDialog = false">取 消</el-button>
+                <el-button type="primary" @click="showAddDialog = false">保 存</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <script>
+    import userManage from './管理员设置-角色管理.vue'
     export default {
+        components: {
+          userManage  
+        },
         data() {
             return {
                 activeTab: 'admin',
+                formLabelWidth: '160px',
+                showAddDialog: false,
                 admin: {
                     currentPage: 1,
-                    data:[{
-                        accout:'123218',
+                    data: [{
+                        accout: '123218',
                         name: 'guyiyi',
                         phone: '188888',
                         wechat: '123213',
                         create_time: '2011-11-10',
                         postion: '性奴',
-                        control:{
+                        control: {
                             edit: true,
                             permissions: true,
                             delete: true
                         }
-                    }]
+                    }],
+                    addData: {}
                 }
             };
         },
         methods: {
             tabChange(tab, event) {
-                console.log(tab, event);
+                // console.log(tab, event);
             }
         }
     };
