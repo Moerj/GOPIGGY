f6556c086b395f8a82f9d3ba00dfe806f4d01cf5
Moer <199525807@qq.com>
2017-04-23 04:00:25 +0800

    修改build打包为相对路径 ./

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/GOPIGGY', // 默认'/',绝对路径,服务器的根目录
+    assetsPublicPath: './', // 默认'/',绝对路径,服务器的根目录
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
