413486c334f7fa95423456dc63da5447401064b1
Moer <199525807@qq.com>
2017-05-03 01:48:34 +0800

    .

--- src/components/财务报表/我的账户.vue
+++ src/components/财务报表/我的账户.vue
@@ -25,7 +25,7 @@
         >li {
             padding-left: 20px;
             position: relative;
-            >.fa {
+            >.fa-icon {
                 position: absolute;
                 top: 5px;
                 left: 0;
@@ -43,22 +43,22 @@
         <el-col :span="8">
             <div class="ui-card">
                 <span>
-                    <span class="color-icon red"><i class="fa fa-rmb"></i></span>
+                    <span class="color-icon red"><icon name="rmb"></icon></span>
                 <span>会员资金管理</span>
                 </span>
                 <ul class="ul-style">
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">会员资金</p>
                         <p class="f-color-grey f-10 m-0">查看会员预存款,额度及交易明细</p>
                     </li>
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">会员资金</p>
                         <p class="f-color-grey f-10 m-0">查看会员预存款,额度及交易明细</p>
                     </li>
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">会员资金</p>
                         <p class="f-color-grey f-10 m-0">查看会员预存款,额度及交易明细</p>
                     </li>
@@ -68,22 +68,22 @@
         <el-col :span="8">
             <div class="ui-card">
                 <span>
-                            <span class="color-icon yellow"><i class="fa fa-bullseye"></i></span>
+                            <span class="color-icon yellow"><icon name="bullseye"></icon></span>
                 <span>千米币</span>
                 </span>
                 <ul class="ul-style">
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">千米币明细</p>
                         <p class="f-color-grey f-10 m-0">查看千米币资金流水明细</p>
                     </li>
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">充值明细</p>
                         <p class="f-color-grey f-10 m-0">查看充值明细</p>
                     </li>
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">提现明细</p>
                         <p class="f-color-grey f-10 m-0">查看提现明细</p>
                     </li>
@@ -93,22 +93,22 @@
         <el-col :span="8">
             <div class="ui-card">
                 <span>
-                            <span class="color-icon blue"><i class="fa fa-address-book"></i></span>
+                            <span class="color-icon blue"><icon name="address-book"></icon></span>
                 <span>设置</span>
                 </span>
                 <ul class="ul-style">
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">银行卡</p>
                         <p class="f-color-grey f-10 m-0">添加提现银行卡</p>
                     </li>
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">支付密码</p>
                         <p class="f-color-grey f-10 m-0">设置、修改、重置支付密码</p>
                     </li>
                     <li>
-                        <i class="fa fa-circle"></i>
+                        <icon name="circle" scale=".5"></icon>
                         <p class="m-b-5">收款方式</p>
                         <p class="f-color-grey f-10 m-0">查看及设置各种支付方式</p>
                     </li>
