cb017c4ac581ba862a58de87d27b871471127615
Moer <199525807@qq.com>
2017-05-02 15:03:15 +0800

    调整main

---
+++ src/config/directive.js
@@ -0,0 +1,5 @@
+import requireAll from './requireAll'
+
+// 加载组件、指令、配置
+// 参数(模块文件夹路径, 是否包含子目录, 文件匹配的正则)
+export default requireAll(require.context("src/directive", true, /^\.\/.*\.js$/));
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App'
-import requireAll from './config/requireAll'
 
 // http请求
 import axios from './config/axios'
 Vue.use(axios)
-
 
 // element UI
 import ElementUI from 'element-ui'
@@ -15,10 +13,12 @@ Vue.use(ElementUI)
 // fa字体库
 import 'assets/font-awesome/scss/font-awesome.scss'
 
-
 // 自定义样式
 import 'src/sass/layout/base.scss' //快捷样式库
 import 'src/sass/layout/ui.scss' //ui样式
+
+// 自定义全局指令
+import './config/directive'
 
 // 公共组件
 import dropsearch from 'components/global/dropsearch'
@@ -26,13 +26,7 @@ import imgupload from 'components/global/imgupload'
 Vue.component('ui-dropsearch', dropsearch)
 Vue.component('ui-img-upload', imgupload)
 
-
-// 加载组件、指令、配置
-// 参数(模块文件夹路径, 是否包含子目录, 文件匹配的正则)
-requireAll(require.context("components/directive", true, /^\.\/.*\.js$/));
-
-
-// 引入路由
+// 路由集合
 import router from './config/router'
 
 
